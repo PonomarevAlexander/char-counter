db6bf5360e5002ede7c496e6eb53674cb4f08157
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2021-01-17 16:17:48 +0300

    renamed Map

--- src/main/java/org/foxminded/charcounter/facade/CharCounter.java
+++ src/main/java/org/foxminded/charcounter/facade/CharCounter.java
@@ -12,7 +12,7 @@ public class CharCounter {
     private Counter counter = new Counter();
     private WordStorage storage = new WordStorage();
     private Formatter formatter = new Formatter();
-    private Map<Character, Integer> resultData = new HashMap<>();
+    private Map<Character, Integer> totalCharsAmount = new HashMap<>();
 
     private CharCounter() {
     }
@@ -35,11 +35,11 @@ public class CharCounter {
                 storage.putToStorage(subString, counter.toCountCharacters(subString));
             }
         }
-        return formatter.formatToPrint(resultData).toString();
+        return formatter.formatToPrint(totalCharsAmount).toString();
     }
     
     private void mergeAmount(Map<Character, Integer> data) {
-        data.forEach((letter, amount) -> resultData.computeIfPresent(letter, (key, value) -> amount + value));
-        data.forEach((letter, amount) -> resultData.putIfAbsent(letter, amount));   
+        data.forEach((letter, amount) -> totalCharsAmount.computeIfPresent(letter, (key, value) -> amount + value));
+        data.forEach((letter, amount) -> totalCharsAmount.putIfAbsent(letter, amount));   
     }
 }
