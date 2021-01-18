7dfbec67467de913bbbeaaa3168e6fa42af214d9
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2021-01-19 01:31:10 +0300

    implement test cases

---
+++ src/test/java/org/foxminded/charcounter/facade/CharCountTest.java
@@ -0,0 +1,83 @@
+package org.foxminded.charcounter.facade;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mock;
+
+import org.foxminded.charcounter.gears.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import static org.mockito.Mockito.*;
+import java.util.Map;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CharCountTest {
+    
+    private static final String SOURCE_STRING = "there is facade test";
+    private static final String EXPECTED = "\" \" - 3\n"
+            + "\"a\" - 2\n"
+            + "\"r\" - 1\n"
+            + "\"s\" - 2\n"
+            + "\"c\" - 1\n"
+            + "\"t\" - 3\n"
+            + "\"d\" - 1\n"
+            + "\"e\" - 4\n"
+            + "\"f\" - 1\n"
+            + "\"h\" - 1\n"
+            + "\"i\" - 1\n";
+
+    @Mock
+    private Splitter splitter;
+    
+    @Mock
+    private Counter counter;
+    
+    @Mock
+    private WordStorage storage;
+    
+    @Mock
+    private Formatter formatter;
+    
+    @Mock
+    private Map<Character, Integer> charsAmountStubb;
+    
+    @InjectMocks
+    private CharCount charCount = new CharCount();
+    
+    
+    @Test
+    void testTimesInvokationMethodsWithoutLoop() {
+        charCount.countCharacters(SOURCE_STRING);
+        
+        verify(splitter).splitString(SOURCE_STRING);
+        verify(counter).countSpecSymbols(SOURCE_STRING);
+        verify(formatter).formatToPrint(charsAmountStubb);
+    }
+    
+    @Test
+    void testOrderInvokeMethodsWhithoutLoop() {
+        InOrder inOrder = inOrder(splitter, counter, formatter);
+        charCount.countCharacters(SOURCE_STRING);
+        inOrder.verify(splitter).splitString(SOURCE_STRING);
+        inOrder.verify(counter).countSpecSymbols(SOURCE_STRING);
+        inOrder.verify(formatter).formatToPrint(charsAmountStubb);
+    }
+    
+    @Test
+    void testCountCharactersShouldReturnCorrectResult() {
+        CharCount cc = new CharCount();
+        String actual = cc.countCharacters(SOURCE_STRING);
+        assertEquals(EXPECTED, actual);
+    }
+}
+
+
+
+
+
+
+
+
